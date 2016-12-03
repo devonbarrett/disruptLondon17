eaea44a29adbcd9e6c2547e624da1b51337df67e
Devon Barrett <devon@devonbarrett.net>
2016-12-03 15:59:40 +0000

    test2

--- sparkbot/app.js
+++ sparkbot/app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
+var bot = require('./routes/sparkbot');
 
 var app = express();
 
@@ -15,6 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/', index);
+app.use('/flint', bot);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ sparkbot/routes/sparkbot.js
@@ -0,0 +1,30 @@
+var Flint = require('node-flint');
+var webhook = require('node-flint/webhook');
+var express = require('express');
+var router = express.Router();
+
+// flint options
+var config = {
+  webhookUrl: 'http://app.spoken.tech/flint',
+  token: '***REMOVED***',
+  port: 80
+};
+
+// init flint
+var flint = new Flint(config);
+flint.start();
+
+// say hello
+flint.hears('/start', function(bot, trigger) {
+  bot.say('Hello %s!', trigger.personDisplayName);
+});
+
+// define express path for incoming webhooks
+router.post('/', webhook(flint));
+
+process.on('SIGINT', function() {
+  flint.stop().then(function() {
+    process.exit();
+  });
+});
+
